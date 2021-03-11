d51e3482ebc7f8d8f56fd040dfdfaf3f76bd1a90
Amit Aryeh Levy <amit@amitlevy.com>
2021-03-10 18:39:24 -0800

    Device tree working well enough to initial UART

--- src/device_tree.rs
+++ src/device_tree.rs
@@ -28,7 +28,7 @@ impl fmt::LowerHex for BE {
 }
 
 #[repr(C)]
-pub struct Header {
+pub struct DeviceTree {
     magic: BE,
     total_size: BE,
     dt_struct_offset: BE,
@@ -41,7 +41,7 @@ pub struct Header {
     dt_struct_size: BE,
 }
 
-impl fmt::Display for Header {
+impl fmt::Debug for DeviceTree {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.write_str("Header {\n")?;
         write!(f, "  magic: {:#x}\n", self.magic)?;
@@ -66,18 +66,30 @@ impl fmt::Display for Header {
     }
 }
 
-impl Header {
-    pub unsafe fn nodes(&self) -> NodeIterator {
-        NodeIterator {
-            struct_base: (self as *const _ as *const u8)
-                .offset(self.dt_struct_offset.native() as isize)
-                as *const BE,
-            strings_base: (self as *const _ as *const u8)
-                .offset(self.dt_strings_offset.native() as isize),
-            depth: 0,
-            _phantom: &(),
+impl DeviceTree {
+    pub unsafe fn from_address<'a>(base: *const Self) -> &'a Self {
+        &*base
+    }
+
+    pub fn nodes(&self) -> NodeIterator {
+        unsafe {
+            NodeIterator {
+                struct_base: (self as *const _ as *const u8)
+                    .offset(self.dt_struct_offset.native() as isize)
+                    as *const BE,
+                strings_base: (self as *const _ as *const u8)
+                    .offset(self.dt_strings_offset.native() as isize),
+                depth: 0,
+                search_depth: 0,
+                _phantom: &(),
+            }
         }
     }
+
+    pub fn root(&self) -> Option<Node> {
+        self.nodes().next()
+    }
+
 }
 
 #[repr(C)]
@@ -89,50 +101,113 @@ pub struct PropHeader {
 
 #[derive(Debug)]
 pub struct Prop<'a> {
-    header: &'a PropHeader,
-    val: &'a [u8],
-    name: &'a str,
+    pub name: &'a [u8],
+    pub value: &'a [u8],
 }
 
-#[derive(Debug)]
+pub struct PropIterator<'a> {
+    struct_base: *const BE,
+    strings_base: *const u8,
+    _phantom: &'a (),
+}
+
+impl<'a> Iterator for PropIterator<'a> {
+    type Item = Prop<'a>;
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            loop {
+                let val = (*self.struct_base).native();
+                self.struct_base = self.struct_base.offset(1);
+                match val {
+                    0x4 => {},
+                    0x1 => return None,
+                    0x2 => return None,
+                    0x9 => return None,
+                    0x3 => {
+                        let prop = &*(self.struct_base as *const PropHeader);
+                        let val_base = (prop as *const PropHeader).offset(1) as *const u8;
+
+                        let string_base = self.strings_base.offset(prop.nameoff.native() as isize);
+                        let mut string_len = 0;
+                        let mut cur = string_base;
+                        while *cur != 0 {
+                            cur = cur.offset(1);
+                            string_len += 1;
+                        }
+
+                        self.struct_base = {
+                            let len = prop.len.native() as usize;
+                            let mut ptr = (val_base as usize) + len;
+                            ptr = (ptr + 4 - 1) & !3;
+                            ptr as *const BE
+                        };
+
+                        let fullp = Prop {
+                            value: core::slice::from_raw_parts(val_base, prop.len.native() as usize),
+                            name: core::slice::from_raw_parts(string_base, string_len),
+                        };
+                        return Some(fullp);
+                    }
+                    _ => panic!("WTF"),
+                }
+            }
+        }
+    }
+}
+
+#[derive(Copy,Clone,Debug)]
 pub struct Node<'a> {
     strings_base: *const u8,
     base: *const BE,
-    pub name: &'a str,
+    pub name: &'a [u8],
     pub depth: usize,
 }
 
 impl<'a> Node<'a> {
-    pub fn prop(&self, _i: usize) -> Option<Prop> {
-        unsafe {
-            let mut struct_base = self.base.offset(1);
-            loop {
-                let val = (*struct_base).native();
-                struct_base = struct_base.offset(1);
-                if val == 0x3 {
-                    break
-                } else if val == 0x2 {
-                    return None;
-                }
-            }
-            let prop = &*(struct_base as *const PropHeader);
-            let val_base = (prop as *const PropHeader).offset(1) as *const u8;
-
-            let string_base = self.strings_base.offset(prop.nameoff.native() as isize);
-            let mut string_len = 0;
-            let mut cur = string_base;
-            while *cur != 0 {
-                cur = cur.offset(1);
-                string_len += 1;
-            }
-
-            let fullp = Prop {
-                header: prop,
-                val: core::slice::from_raw_parts(val_base, prop.len.native() as usize),
-                name: core::str::from_utf8_unchecked(core::slice::from_raw_parts(string_base, string_len)),
-            };
-            Some(fullp)
+    pub fn props(&self) -> PropIterator<'a> {
+        PropIterator {
+            struct_base: self.base,
+            strings_base: self.strings_base,
+            _phantom: &(),
         }
+    }
+
+    pub fn prop_by_name(&self, name: &str) -> Option<Prop> {
+        let name = name.as_bytes();
+        self.props().find(|prop| prop.name == name)
+    }
+
+    pub fn children(&self) -> NodeIterator<'a> {
+        NodeIterator {
+            struct_base: self.base,
+            strings_base: self.strings_base,
+            depth: self.depth,
+            search_depth: 0,
+            _phantom: &(),
+        }
+    }
+
+    pub fn child_by_name(&self, name: &str) -> Option<Node> {
+        let name = name.as_bytes();
+        self.children().find(|node| {
+            node.name == name
+        })
+    }
+
+    fn child_by_path_helper<'b, I: Iterator<Item = &'b [u8]>>(self, mut path: I) -> Option<Node<'a>> {
+        path.next().and_then(|cur|
+            self.children().find(|node| node.name == cur).and_then(|next|
+               next.child_by_path_helper(path)
+            )
+        ).or(Some(self))
+    }
+
+    pub fn child_by_path<B: 'a + AsRef<[u8]>>(self, name: B) -> Option<Node<'a>> {
+        let mut path = name.as_ref().split(|c| *c == b'/');
+        if name.as_ref().first() == Some(&b'/') {
+            path.next();
+        }
+        self.child_by_path_helper(path)
     }
 }
 
@@ -140,6 +215,7 @@ pub struct NodeIterator<'a> {
     struct_base: *const BE,
     strings_base: *const u8,
     depth: usize,
+    search_depth: usize,
     _phantom: &'a (),
 }
 
@@ -156,7 +232,7 @@ impl<'a> Iterator for NodeIterator<'a> {
                 match val {
                     0x1 => {
                         /* Begin node */
-                        let base = self.struct_base;
+                        //let base = self.struct_base;
                         let name_base = self.struct_base as *const u8;
                         let mut name_base_cur = name_base;
                         let mut name_len = 0;
@@ -167,14 +243,19 @@ impl<'a> Iterator for NodeIterator<'a> {
                         let mut next_base_i = (name_base_cur as usize) + 1; // one past the null byte
                         next_base_i = (next_base_i + 4 - 1) & !3;
                         self.struct_base = next_base_i as *const BE;
-                        let name = core::str::from_utf8_unchecked(core::slice::from_raw_parts(name_base, name_len));
-                        let depth = self.depth;
-                        self.depth += 1;
-                        return Some(Node { base, name, strings_base: self.strings_base, depth });
+                        let name = core::slice::from_raw_parts(name_base, name_len);
+                        self.search_depth += 1;
+                        if self.search_depth == 1 {
+                            let depth = self.depth + 1;
+                            return Some(Node { base: self.struct_base, name, strings_base: self.strings_base, depth });
+                        }
                     },
                     0x2 => {
                         /* End node */
-                        self.depth -= 1;
+                        if self.search_depth == 0 {
+                            return None
+                        }
+                        self.search_depth -= 1;
                     },
                     0x3 => {
                         /* Property */
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use core::str;
 
 mod device_tree;
 
+#[cfg(target_arch = "aarch64")]
 global_asm!(include_str!("boot.S"));
 
 use core::panic::PanicInfo;
@@ -25,16 +26,61 @@ impl core::fmt::Write for UART {
     }
 }
 
-#[no_mangle]
-pub extern "C" fn kernel_main(dtb: &device_tree::Header) {
-    let mut uart = UART(0x0900_0000 as *mut u8);
-    for node in unsafe { dtb.nodes() } {
-        for _ in 0..node.depth {
-            let _ = write!(&mut uart, "  ");
-        }
-        let _ = write!(&mut uart, "{}\n", node.name);
+fn null_terminated_str(bytes: &[u8]) -> &[u8] {
+    if bytes[bytes.len() - 1] == 0 {
+        &bytes[..bytes.len() - 1]
+    } else {
+        bytes
     }
-    let _ = write!(&mut uart, "{}\n", dtb );
+}
+
+fn regs_to_usize(regs: &[u8], cell_size: usize) -> (usize, &[u8]) {
+    let mut result = 0;
+    let (work, rest) = regs.split_at(cell_size * 4);
+    for chunk in work.chunks(4) {
+        let mut c = [0; 4];
+        c.copy_from_slice(chunk);
+        result = result << 32 | (u32::from_be_bytes(c) as usize);
+    }
+    (result, rest)
+}
+
+#[no_mangle]
+pub extern "C" fn kernel_main(dtb: &device_tree::DeviceTree) {
+    let mut uart = None;
+    if let Some(root) = dtb.root() {
+        let size_cell = root.prop_by_name("#size-cells").map(|sc| {
+            let mut buf = [0; 4];
+            buf.copy_from_slice(sc.value);
+            u32::from_be_bytes(buf) as usize
+        }).unwrap_or(2);
+        let address_cell = root.prop_by_name("#address-cells").map(|sc| {
+            let mut buf = [0; 4];
+            buf.copy_from_slice(sc.value);
+            u32::from_be_bytes(buf) as usize
+        }).unwrap_or(2);
+
+        if let Some(chosen) = root.child_by_name("chosen") {
+            chosen.prop_by_name("stdout-path")
+                .map(|stdout_path| null_terminated_str(stdout_path.value))
+                .filter(|stdout_path| stdout_path == b"/pl011@9000000")
+                .map(|stdout_path| {
+                    root.child_by_path(stdout_path)
+                        .map(|stdout| {
+                            if let Some(reg) = stdout.prop_by_name("reg") {
+                                let (addr, rest) = regs_to_usize(reg.value, address_cell);
+                                let (size, _) = regs_to_usize(rest, size_cell);
+                                if size == 0x1000 {
+                                    uart = Some(UART(addr as *mut u8));
+                                }
+                            }
+                        });
+                });
+        }
+        uart.as_mut().map(|uart| {
+            let _ = write!(uart, "We booted!\n");
+        });
+    }
 }
 
 #[panic_handler]
